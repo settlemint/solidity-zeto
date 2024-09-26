21d11a375a35b0ca0373de9eaddc8614e8074a66
Roderik van der Veer <roderik@settlemint.com>
2024-09-26 21:23:33 +0200

    feat: update subgraph

--- subgraph/fetch/account.ts
+++
@@ -1,8 +0,0 @@
-import { Address, Bytes } from "@graphprotocol/graph-ts";
-import { Account } from "../../generated/schema";
-
-export function fetchAccount(address: Address): Account {
-  const account = new Account(Bytes.fromHexString(address.toHex()));
-  account.save();
-  return account;
-}
